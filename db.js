f085124f38a0f939f8698f97608f3a67cb6cd280
Hasan <syedhasanali931@gmail.com>
2025-07-04 11:20:28 +0530

    signup EP added, tested on PM

--- db.js
+++ db.js
@@ -6,5 +6,13 @@ let productsSchema = new Schema ({
 name : {type : String,required :true}, 
 category : { type : String, enum : ["In Stock", "Out of Stock", "Low Stock", "Purchase"], required : true},
 });
+
+let usersSchema = new Schema ({
+    name : {type: String},
+    email: {type: String, required : true},
+    password : {type : String, required: true}
+    
+})
 const Products = mongoose.model ('Products', productsSchema)
-module.exports = Products;    
+const Users = mongoose.model ("Users",usersSchema)
+module.exports = {Products, Users};    
--- server.js
+++ server.js
@@ -1,26 +1,88 @@
 require('dotenv').config();
 const express = require ("express")
 const mongoose = require ("mongoose")
-let Products = require ('./db')
+let {Products,Users} = require ('./db')
 const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
 
 async function main() {
         try {
           await mongoose.connect(process.env.MONGO_URI);
           console.log("Connected to MongoDB");
-      
-          app.listen(3000, () => {
-            console.log("Server is running on port 3000");
-          });
-      
+  
         } catch (error) {
           console.error("Error connecting to MongoDB:", error);
         }
       }
     
       main();
-const cors = require ("cors")
 
-const app = express (); 
+
+
+const app = express();
+app.use(cors()); // Enable all CORS requests
+app.use (express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+
+app.post("/new-user-signup", async (req,res)=> {
+let name=req.body.name;
+let email = req.body.email;
+let password = req.body.password;
+
+//t/c block
+try {
+const userEntry = await Users.create ({
+  name,email,password
+})
+
+res.status(201).json({ 
+    success: true,
+    user: {
+      id: userEntry._id,
+      name: userEntry.name,
+      email: userEntry.email
+     
+    }
+  });
+}
+catch (error) {
+    console.error("Detailed error:", error);  // Add this line
+    res.json({
+        error: "Entry was not posted",
+        details: error.message  // Include the actual error message
+    })
+}
+//ends
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 
